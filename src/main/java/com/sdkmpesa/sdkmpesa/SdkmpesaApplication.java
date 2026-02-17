beeb2eee8d9b2b42873a44f472b24cfa6e6f6685
Ciamuthama <muthamapeter79@gmail.com>
2026-02-17 09:57:55 +0300

    Initial commit

---
+++ src/main/java/com/sdkmpesa/sdkmpesa/SdkmpesaApplication.java
@@ -0,0 +1,13 @@
+package com.sdkmpesa.sdkmpesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkmpesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdkmpesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdkmpesa/sdkmpesa/SdkmpesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdkmpesa.sdkmpesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdkmpesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
